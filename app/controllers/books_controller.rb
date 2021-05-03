56dd801c6c486e18390e23ede966e5d7a05a39a4
“Masatooo0” <masato.n.0321@gmail.com>
2021-05-03 08:06:26 +0000

    [edit]レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,21 @@
 class BooksController < ApplicationController
     def index
         @books = Book.all
+        @book = Book.new
     end
 
     def show
         @book = Book.find(params[:id])
     end
 
-    def new
-        @book = Book.new
-    end
-
     def create
         @book = Book.new(book_params)
-        @book.save
-        redirect_to books_path
+        if @book.save
+            flash[:notice] = "Book was successfully created."
+            redirect_to book_path(@book)
+        else
+            redirect_to book_path(@book)
+        end
     end
 
     def edit
@@ -23,14 +24,22 @@ class BooksController < ApplicationController
 
     def update
         book = Book.find(params[:id])
-        book.update(book_params)
-        redirect_to book_path(book)
+        if book.update(book_params)
+            flash[:notice] = "Book was successfully updated."
+            redirect_to book_path(book)
+        else
+            redirect_to book_path(book)
+        end
     end
 
     def destroy
         book = Book.find(params[:id])
-        book.destroy
-        redirect_to books_path
+        if book.destroy
+            flash[:notice] = "Book was successfully destroyed."
+            redirect_to books_path
+        else
+            redirect_to books_path
+        end
     end
 
     private
